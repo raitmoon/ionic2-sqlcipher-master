de5cd235ecf9d4bc2ba54be4f4a883868f45a5e7
Tomonari Okubo <tomonariokubo@Tomonari-no-MacBook-Pro.local>
2016-05-06 23:21:18 +0900

    add comment

--- app/pages/list/list.ts
+++ app/pages/list/list.ts
@@ -1,4 +1,6 @@
 import {Page, NavController, NavParams, Platform} from 'ionic-angular';
+
+//プラグインの読み込み
 declare var sqlitePlugin:any;
 declare var plugins:any;
 declare var cordova:any;
@@ -9,19 +11,26 @@ declare var window:any;
 })
 export class ListPage {
 
+  //配列の定義
   items: Array<{title: string}>;
 
   constructor(private nav: NavController, navParams: NavParams, platform: Platform) {
+    //DeviceReadyになってから処理を開始（ここが重要）
     platform.ready().then(()=>{
+      //DBファイルの物理パスを作成
       var filePath : string = cordova.file.dataDirectory.replace("NoCloud", "LocalDatabase") + 'encrypted.db';
+      //ファイルの存在チェック
       window.resolveLocalFileSystemURI(filePath, ()=> {
+        //DBファイルがある場合は、データ取得処理を実行
         this.getData();
       }, (error) => {
+        //DBファイルがない場合(初回起動時)はDBファイルのコピー処理を実行
         this.dbCopy();
       })
     });
   }
   
+  //DBファイルコピー処理
   dbCopy() {
     plugins.sqlDB.copy('encrypted.db', 2, ()=>{
       this.getData();
@@ -30,35 +39,42 @@ export class ListPage {
     });
   }
 
+  //データ取得処理
   getData(){
+    //データベース接続をオープン
     sqlitePlugin.openDatabase({name: 'encrypted.db', key: 'Password', location: 'default'}, (db) => {
+      //なぜかトランザクション処理しないとSELECTできないのでトランザクション開始
       db.transaction((tx) => {
-          var query: string = "SELECT * FROM items";
-          //var param: [string] = ['param'];
-          this.items = [];
-          tx.executeSql(query, [], (tx, resultSet) => {
-            for (var i: number = 0; i < resultSet.rows.length; i++ ) {
-              //alert("name: " + resultSet.rows.item(i).name);
-              this.items.push({
-                title: resultSet.rows.item(i).name
-              });
-            }
-          }, (error) => {
-            alert('SELECT error: ' + error.message);
-            console.log('SELECT error: ' + error.message);
-          });
+        //SELECT文を作成
+        var query: string = "SELECT * FROM items";
+        //var param: [string] = ['param'];
+        this.items = [];
+        //SQL実行
+        tx.executeSql(query, [], (tx, resultSet) => {
+          for (var i: number = 0; i < resultSet.rows.length; i++ ) {
+            //items配列へデータを追加
+            this.items.push({
+              title: resultSet.rows.item(i).name
+            });
+          }
         }, (error) => {
-          alert('transaction error: ' + error.message);
-          console.log('transaction error: ' + error.message);
-        }, () => {
-          console.log('transaction ok');
+          alert('SELECT error: ' + error.message);
+          console.log('SELECT error: ' + error.message);
         });
       }, (error) => {
-        alert('error' + error.message);
+        alert('transaction error: ' + error.message);
+        console.log('transaction error: ' + error.message);
+      }, () => {
+        console.log('transaction ok');
+      });
+    }, (error) => {
+      alert('error' + error.message);
     });
   }  
 
+  //アイテムがタップされた時の処理
   itemTapped(event, item) {
+    //ListPage(自分自身)をプッシュして表示
     this.nav.push(ListPage, {
       item: item
     });
